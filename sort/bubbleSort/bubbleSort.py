b1b995b7868dfd6d65b64d41fc891bce4a52ca20
盛佳 <1368329355@qq.com>
2019-09-17 16:50:06 +0800

    [bubble sort] python

--- sort/bubbleSort/bubbleSort.py
+++ sort/bubbleSort/bubbleSort.py
@@ -2,9 +2,19 @@ data = [2, 1, 7, 5, 6, 3, 2, 0, 9]
 
 
 def bubbleSort(data):
-    for i in range(0, len(data)):
-        if data[i] > data[i+1]:
-            print(data[i])
+    n = len(data)
+    # The out layer loop control times from head to end
+    for j in range(n-1):
+        # use count to record the counts about all exchange
+        count = 0
+        # the inner layer loop control times
+        for i in range(0, n-1-j):
+            if data[i] > data[i+1]:
+                data[i], data[i+1] = data[i+1], data[i]
+                count += 1
+        if 0 == count:
+            break
+    return data
 
 
-bubbleSort(data)
+print(bubbleSort(data))
