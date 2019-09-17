b15954bcfada456764848c116bbade0ebff7b666
盛佳 <1368329355@qq.com>
2019-09-17 14:58:09 +0800

    [sort] bubble sort

--- sort/bubbleSort/bubbleSort.php
+++ sort/bubbleSort/bubbleSort.php
@@ -3,12 +3,20 @@
  * Date: 2019/9/16
  * Time: 11:05
  * @param array $data
+ * @return array
  */
 
 function bubbleSort(array $data)
 {
     $count = count($data);
-    print_r($data);
+    for ($i = 1; $i < $count; $i++) {
+        for ($j = 0; $j < $count - $i; $j++) {
+            if ($data[$j] > $data[$j + 1]) {
+                list($data[$j], $data[$j + 1]) = [$data[$j + 1], $data[$j]];
+            }
+        }
+    }
+    return $data;
 }
 
-bubbleSort([2,9,1,5,6,0,3,4]);
+print_r(bubbleSort([0, 2, 9, 1, 5, 6, 0, 3, 4]));
