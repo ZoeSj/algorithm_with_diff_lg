ad06b893e2742b951614106cbd0c73e0573c19c6
盛佳 <1368329355@qq.com>
2019-09-17 14:21:41 +0800

    git init

---
+++ sort/bubbleSort/bubbleSort.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Date: 2019/9/16
+ * Time: 11:05
+ * @param array $data
+ */
+
+function bubbleSort(array $data)
+{
+    $count = count($data);
+    print_r($data);
+}
+
+bubbleSort([2,9,1,5,6,0,3,4]);
